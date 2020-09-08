aab6a539450a97daacdd3550091f8f41710d6487
Paul <phapp1988@gmail.com>
2020-09-08 16:15:16 -0500

    Updates

---
+++ precache-manifest.c37288567c9c9d8f4fcaf52812b7fab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda64c21165aa2c1f35f4f82d891ebb5",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "84704a29216ff09221c9",
+    "url": "/game-of-life/static/css/main.2f6b8a27.chunk.css"
+  },
+  {
+    "revision": "f4bf6c9b522b1d6403ef",
+    "url": "/game-of-life/static/js/2.43f8c2a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-of-life/static/js/2.43f8c2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84704a29216ff09221c9",
+    "url": "/game-of-life/static/js/main.f3da7b2b.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
